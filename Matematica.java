2b87a5f8e0c9f864e46bc78c476b046818a4d8b6
Lelis-Alves <elias.ti.alves@outlook.com>
2020-05-29 13:54:57 -0300

    Teste de exemplo com jenkis

---
+++ Matematica.java
@@ -0,0 +1,19 @@
+
+
+public class Matematica {
+
+	public static int soma(int a, int b) {
+		return a + b;
+
+	}
+
+
+	public static void main(String[] args) {
+
+		int total = soma(1,3);
+
+		System.out.println(total);
+
+	}
+
+}
